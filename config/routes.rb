0d09c93674d980c4fe12c42546d18fd0e998155d
Justin Schoen <jaschoen@gmail.com>
2015-08-25 17:30:01 -0500

    added acts_as_votable

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :links
+  resources :links do
+    member do
+      put "upvote",    to: "links#upvote"
+      put "downvote", to: "links#downvote"
+    end
+  end
   root to: "links#index"
 end
